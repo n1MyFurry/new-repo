57724b2a58e7ce66b26a522bced4a37f265ae189
n1MyFurry <k.github@yandex.ru>
2023-07-30 15:32:17 +0300

    another one2

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
-import NextAuth from "next-auth";
-import { authConfig } from "@/config/auth";
+import NextAuth from 'next-auth';
 
-const handler = NextAuth(authConfig);
+import { authOptions } from '@/lib/session';
 
-export { handler as GET, handler as POST }
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,82 @@
-"use client";
+import { ProjectInterface } from '@/common.types';
+import Categories from '@/components/Categories';
+import LoadMore from '@/components/LoadMore';
+import ProjectCard from '@/components/ProjectCard';
+import { fetchAllProjects } from '@/lib/actions';
+import React from 'react';
 
-import { useSession } from "next-auth/react";
-import Link from "next/link";
-import React from "react";
-
-export default function Home() {
-
- const session = useSession();
-
-  console.log(session);
-  
-
-  return (
-    <main className="flex items-center justify-center w-full h-full">
-      <div className="w-72 h-72 bg-black/10 rounded-lg">
-        {session?.data ? (
-          <Link href="/api/auth/signout">
-            Sign Out
-          </Link>
-        ): (
-          <Link href="/api/auth/signin">
-            Sign In
-          </Link>
-        )}
-        <div>
-          {session?.data?.user?.email}
-        </div>
-        <br />
-        <Link href="/profile">
-          Profile
-        </Link>
-      </div>
-    </main>
-  )
+type ProjectSearch = {
+    projectSearch: {
+        edges: { node: ProjectInterface }[];
+        pageInfo: {
+            hasPreviousPage: boolean;
+            hasNextPage: boolean;
+            startCursor: string;
+            endCursor: string;
+        }
+    }
 }
+
+type SearchParams = {
+    category?: string;
+    endcursor?: string;
+}
+
+type Props = {
+    searchParams: SearchParams;
+}
+
+export const dynamic = 'force-dynamic';
+export const dynamicParams = true;
+export const revalidate = 0;
+
+const Home = async ({ searchParams: { category, endcursor } }: Props) => {
+
+    return (
+        <div>
+            Hello world
+        </div>
+    )
+
+    const data = await fetchAllProjects(category, endcursor) as ProjectSearch;
+
+    const projectsToDisplay = data?.projectSearch?.edges || [];
+
+    if(projectsToDisplay.length === 0) {
+        return (
+            <section className="flexStart flex-col paddings">
+                <Categories />
+                <p className="no-result-text text-center">No projects found, go create some first.</p>
+            </section>
+        );
+    }
+
+    const pagination = data?.projectSearch?.pageInfo;
+
+    return (
+        <section className="flex-start flex-col paddings mb-16">
+            <Categories />
+            <section className="projects-grid">
+                {projectsToDisplay.map(({ node }: { node: ProjectInterface }) => (
+                    <ProjectCard 
+                        key={node?.id}
+                        id={node?.id}
+                        image={node?.image}
+                        title={node?.title}
+                        name={node?.createdBy?.name}
+                        avatarUrl={node?.createdBy?.avatarUrl}
+                        userId={node?.createdBy?.id}
+                    />
+                ))}
+            </section>
+            <LoadMore
+                startCursor={pagination.startCursor}
+                endCursor={pagination.endCursor}
+                hasPreviousPage={pagination.hasPreviousPage}
+                hasNextPage={pagination.hasNextPage}
+            />
+        </section>
+    );
+};
+
+export default Home;
